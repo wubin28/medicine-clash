1b076af11cbcf5dd98a3f66fd8360944879a0141
vulture_D5D44E2249 <vulture@cyber-dojo.org>
2016-01-14 17:24:42 +0000

    32

--- sandbox/Patient.java
+++ sandbox/Patient.java
@@ -3,4 +3,7 @@ import java.util.List;
 public class Patient {
     public Patient(List<Medicine> medicines) {
     }
+
+    public int clash(List<String> medicineNames, int daysBeforeToday) {
+    }
 }
