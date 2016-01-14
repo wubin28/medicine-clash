afcd121f4908115b08e874147d3443e705e4897a
vulture_D5D44E2249 <vulture@cyber-dojo.org>
2016-01-14 18:50:34 +0000

    49

--- sandbox/Prescription.java
+++ sandbox/Prescription.java
@@ -2,12 +2,18 @@ import java.time.LocalDate;
 
 public class Prescription {
     private LocalDate dispenseDate;
+    private int daysSupply;
 
     public Prescription(LocalDate dispenseDate, int daysSupply) {
         this.dispenseDate = dispenseDate;
+        this.daysSupply = daysSupply;
     }
 
     public LocalDate getDispenseDate() {
         return this.dispenseDate;
+    }
+
+    public int getDaysSupply() {
+        return this.daysSupply;
     }
 }
