2cbf15281ca92addf4d5fa18aa2e5747cacb8fba
vulture_D5D44E2249 <vulture@cyber-dojo.org>
2016-01-14 18:44:30 +0000

    45

--- sandbox/Medicine.java
+++ sandbox/Medicine.java
@@ -1,6 +1,13 @@
 import java.util.List;
 
 public class Medicine {
+    private List<Prescription> prescriptions;
+
     public Medicine(String name, List<Prescription> prescriptions) {
+        this.prescriptions = prescriptions;
+    }
+
+    public List<Prescription> getPrescriptions() {
+        return this.prescriptions;
     }
 }
