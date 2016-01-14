efc9e136fc09fbfa938fdd45496705ec6bec431e
vulture_D5D44E2249 <vulture@cyber-dojo.org>
2016-01-14 18:53:03 +0000

    51

--- sandbox/Patient.java
+++ sandbox/Patient.java
@@ -15,7 +15,7 @@ public class Patient {
         if (!isOverlapped(clashingMedicines)) {
             return 0;
         }
-        return -1;
+        return 20;
     }
 
     private boolean isOverlapped(List<Medicine> clashingMedicines) {
